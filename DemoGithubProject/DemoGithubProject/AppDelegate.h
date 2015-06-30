190c6faf6840be86230fc449fbee7053f38fecbc
Xiao <xiao@XiaodeMacBook-Pro.local>
2015-07-01 00:50:22 +0800

    submit all files to github

---
+++ DemoGithubProject/DemoGithubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoGithubProject
+//
+//  Created by Xiao on 15/7/1.
+//  Copyright (c) 2015年 Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoGithubProject/DemoGithubProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DemoGithubProject
+//
+//  Created by Xiao on 15/7/1.
+//  Copyright (c) 2015年 Xiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"shirley add commit");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
